57301afaf2c367c6b83980e4358f1cf6a5492cfa
jonathanbe <jonathanbelok@Jonathans-Mac-mini.local>
2016-03-23 23:46:37 -0400

    php contact form

--- index.php
+++ index.php
@@ -297,21 +297,47 @@
                   <a class="close" href="#">&times;</a>
                   <div class="content">
                       
-                    
+                    <!--
                     <input class="contact" placeholder="Name" / >
                     <input class="contact" placeholder="Email" / >
                     <textarea class="contact" placeholder="message" ></textarea>
                     <a class="contact2">Contact</a>
-                   
- <!--                     
-<form action="https://formspree.io/jonathan.on@Hotmail.com"
-      method="POST">
-    <input type="text" name="name">
-    <input type="email" name="_replyto">
-  <textarea type="text" name="message"></textarea>
-    <input type="submit" value="Send">
-</form> -->
+                    -->
+                      
+                      <?php 
+$action=$_REQUEST['action']; 
+if ($action=="")    /* display the contact form */ 
+    { 
+    ?> 
+    <form  action="" method="POST" enctype="multipart/form-data"> 
+    <input type="hidden" name="action" value="submit"> 
 
+    <input class="contact" name="name" type="text" pleaceholer="name"/>
+   
+    <input class="contact" name="email" type="text" placeholder="email"/>
+    
+    <textarea class="contact" name="message" placeholder="message"></textarea> 
+    <input class="contactbutton" type="submit" value="Send email"/> 
+    </form> 
+    <?php 
+    }  
+else                /* send the submitted data */ 
+    { 
+    $name=$_REQUEST['name']; 
+    $email=$_REQUEST['email']; 
+    $message=$_REQUEST['message']; 
+    if (($name=="")||($email=="")||($message=="")) 
+        { 
+        echo "All fields are required, please fill <a href=\"\">the form</a> again."; 
+        } 
+    else{         
+        $from="From: $name<$email>\r\nReturn-path: $email"; 
+        $subject="Message sent using your contact form"; 
+        mail("jonathan.on@Hotmail.com", $subject, $message, $from); 
+        echo "Thank you for contacting me. I will be in touch withing the next 24 hours."; 
+        } 
+    }   
+?> 
                   </div>
                 </div>
               </div>
